8f5b5c27387bb8827d0dbcb2311319145c1dedd1
coldnsteel <85363543+coldnsteel@users.noreply.github.com>
2025-07-19 20:31:45 +0000

    🧹 Waterloo cleanup: Remove HackerWatch-Fortress clones

--- zap-threats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Zapping malware on iPhone 6s+ (011084)..."
-echo "If phone is freezing, go to Settings > General > Transfer or Reset iPhone > Erase All Content and Settings"
-curl -s https://coldnsteel.github.io/HackerWatch/public/intrusion-detection.js | node -e "eval(require('fs').readFileSync(0, 'utf-8'))"
